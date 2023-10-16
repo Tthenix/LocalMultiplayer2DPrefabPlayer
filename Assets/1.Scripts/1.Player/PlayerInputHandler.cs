fe4af3ac5e09cb434cb3b085b5d531d1b6f2d6c0
Nahuel Quiroga <80957556+Tthenix@users.noreply.github.com>
2023-10-16 14:56:51 -0300

    example finish with spawn no ramdom

--- Assets/1.Scripts/1.Player/PlayerInputHandler.cs
+++ Assets/1.Scripts/1.Player/PlayerInputHandler.cs
@@ -10,12 +10,41 @@ public class PlayerInputHandler : MonoBehaviour
     [SerializeField] List<GameObject> prefabs = new List<GameObject>();
     [SerializeField] Transform playerSpawnLeft;
     [SerializeField] Transform playerSpawnRight;
-
+    Vector3 spawnPosition;
+    int orderPlayer = 0;
     private void Start()
     {
+        SpawnerController Sc = FindObjectOfType<SpawnerController>();
+
+        if (Sc.GetCounterPlayer() == 0)
+        {
+            Sc.AddCounterPlayer();
+            int position = Random.Range(0, 2);
+            spawnPosition = position == 0 ? playerSpawnLeft.position : playerSpawnRight.position;
+            if (position == 0)
+            {
+                Sc.Left = true;
+            }
+            orderPlayer = Random.Range(0, prefabs.Count);
+
+            if (orderPlayer == 0)
+            {
+                Sc.Player1 = true;
+            }
+
+        }
+        else
+        {
+            spawnPosition = Sc.Left == false ? playerSpawnLeft.position : playerSpawnRight.position;
+
+            if (Sc.Player1 == true)
+            {
+                orderPlayer = Sc.GetCounterPlayer();
+            }
+            Sc.AddCounterPlayer();
+        }
+        player = GameObject.Instantiate(prefabs[orderPlayer], spawnPosition, Quaternion.identity).GetComponent<Player>();
         // Instancia al jugador en una posición aleatoria (izquierda o derecha).
-        Vector3 spawnPosition = Random.Range(0, 2) == 0 ? playerSpawnLeft.position : playerSpawnRight.position;
-        player = GameObject.Instantiate(prefabs[Random.Range(0, prefabs.Count)], spawnPosition, Quaternion.identity).GetComponent<Player>();
     }
 
     public void Move(InputAction.CallbackContext context)
